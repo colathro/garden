d6a824138fb096a8e5f6aa83b1da14b234845c93
Colton <colathro@microsoft.com>
2021-08-09 19:12:13 -0700

    pip3 instal

--- rasp/setup.py
+++ rasp/setup.py
@@ -1,9 +1,14 @@
 import yaml
 import os
 import git
+import subprocess
 
 global config_file
 config_file = "config.yaml"
+
+
+def install(name):
+    subprocess.call(['pip', 'install', name])
 
 
 def main():
@@ -15,6 +20,7 @@ def main():
         update_code(config_bag)
     else:
         install_code(config_bag)
+    pip_install(config_bag)
     return 0
 
 
@@ -31,6 +37,11 @@ def get_install_directory(config_bag):
 
 def get_home_directory(config_bag):
     return config_bag["pi"]["install-dir"]
+
+
+def pip_install(config_bag):
+    os.chdir(get_install_directory(config_bag))
+    subprocess.call(['pip3', 'install', '-r', 'requirements.txt'])
 
 
 def update_code(config_bag):
