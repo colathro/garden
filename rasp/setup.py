c7fd7c120bd312ae03b8692e1aede1ca513488db
Colton <colathro@microsoft.com>
2021-08-09 22:26:16 -0700

    no need to remove braces?

--- rasp/setup.py
+++ rasp/setup.py
@@ -72,11 +72,11 @@ def update_rc_local(config_bag):
     with open('/etc/rc.local', 'w') as rc_local:
         with open(get_new_rc_local(config_bag), 'r') as new_rc_local:
             content = new_rc_local.read()
-            content = content.replace('{{install_dir}}',
+            content = content.replace('{install_dir}',
                                       get_install_directory(config_bag))
-            content = content.replace('{{target_folder}}',
+            content = content.replace('{target_folder}',
                                       get_target_folder(config_bag))
-            content = content.replace('{{target_entry}}',
+            content = content.replace('{target_entry}',
                                       get_target_entry(config_bag))
         rc_local.writelines(content)
 
